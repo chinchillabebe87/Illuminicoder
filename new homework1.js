8f83f479edbb8e6f080ef9ee377fad80e87aa23a
chinchillabebe87 <nemrifa@yahoo.com>
2024-09-21 16:59:09 +0400

    Add files via upload

---
+++ new homework1.js
@@ -0,0 +1,11 @@
+if(age===1month-12months){
+    console.log (the person is a baby)
+}else if(age===1year=2years){
+    console.log (the person is a toddler)
+}else if(age===3years-12years){
+    console.log (the person is a child)
+}else if(age===13years-17years){
+    console.log(the person is a teenager)
+}else (age===18 years and more){
+    console.log(the person is an adult)
+}
